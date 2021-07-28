0acd34275eea3f67c1c6e02b72c2c97378f10c6b
Whiting <whitinghtennis@gmail.com>
2021-07-27 22:13:04 -0700

    commit tree a79ee2901b5cec04f68cb64bb44968cd28e23f55

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    
+    const timer = setTimeout(() => {
+      if (timeRemaining === 0) {
+            setTimeRemaining(10) 
+            onAnswered(false)
+          } else 
+      setTimeRemaining(timeRemaining - 1)
+    }, 1000);
+    
+    return function() {
+      console.log("cleanup")
+      clearTimeout(timer)    }
+    
+  });
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
